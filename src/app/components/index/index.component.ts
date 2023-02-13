0239f5acfcce74f4745b00f537cbeea52f1462ab
Peter Andreas Moelgaard <gitkraken@petermolgaard.com>
2023-02-13 09:58:32 +0700

    Now with Improved Chrome

--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostBinding} from '@angular/core';
 
 @Component({
   selector: 'app-index',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent {
-
+  // @HostBinding('class')
+  // elementClass = 'w-full';
 }
